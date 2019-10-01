fbc460e28304a2f92b0830b3aabd9129b4d414ad
EugeneZakhno <zohan1234567@gmail.com>
2019-10-01 19:06:41 +0200

    first commit for Task14

---
+++ Task11/src/main/java/Sentence.java
@@ -0,0 +1,22 @@
+
+class Sentence {
+
+    private String sentence;
+
+    Sentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    @Override
+    public String toString() {
+        return sentence;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+}
